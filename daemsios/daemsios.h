26564a233d65d0e67650809bec8deab9e3e10e97
麦志泉 <zhiquan911@qq.com>
2017-04-01 21:24:23 +0800

    搭建完成项目，导入核心库源码

---
+++ daemsios/daemsios.h
@@ -0,0 +1,19 @@
+//
+//  daemsios.h
+//  daemsios
+//
+//  Created by Chance on 2017/4/1.
+//  Copyright © 2017年 Chance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for daemsios.
+FOUNDATION_EXPORT double daemsiosVersionNumber;
+
+//! Project version string for daemsios.
+FOUNDATION_EXPORT const unsigned char daemsiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <daemsios/PublicHeader.h>
+
+
